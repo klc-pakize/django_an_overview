aa12518f3736267158a395bc79534d9d37d2c193
klc-pakize <pa3kize@gmail.com>
2023-03-14 23:16:20 +0300

    feat: created article delete put

--- 1-django_news/news_api/views.py
+++ 1-django_news/news_api/views.py
@@ -35,3 +35,41 @@ def article_list_create_api_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_detail_list_api_view(request): 
+    try:
+        article_instance = Article.objects.get(pk = pk)
+
+    except Article.DoesNotExist:
+        return Response(
+           { "errors": {
+                "code": 404,
+                    "message":"There is no such article"
+                }
+            },
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article_instance)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article_instance, data= request.data)
+        if serializer.is_valid():
+            serializer.save
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        article_instance.delete()
+        return Response(
+           { "message": {
+                "code": 204,
+                    "message":"article deleted"
+                }
+            },
+            status=status.HTTP_204_NO_CONTENT
+        )
