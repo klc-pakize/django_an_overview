4976f9538158a61d15f62e5a7e7acddb323e5e4f
klc-pakize <pa3kize@gmail.com>
2023-03-15 13:58:56 +0300

    feat: created Journalist models re relations

--- 1-django_news/news/models.py
+++ 1-django_news/news/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Journalist(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=150)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f" {self.first_name} {self.last_name} "
 class Article(models.Model):
-    writer = models.CharField(max_length=50)
+    writer = models.ForeignKey(Journalist, on_delete=models.CASCADE, related_name='articles')
     title = models.CharField(max_length=100)
     explanation = models.CharField(max_length=200)
     text = models.TextField()
@@ -17,7 +24,3 @@ class Article(models.Model):
         return self.title
     
 
-class Journalist(models.Model):
-    first_name = models.CharField(max_length=120)
-    last_name = models.CharField(max_length=150)
-    bio = models.TextField(blank=True, null=True)
